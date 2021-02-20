4327a3eab57ea9678dec0b3e390a010ffbed1c88
Maciej Brożek <macibro379@student.polsl.pl>
2021-02-20 18:23:08 +0100

    Changed constrained view to grid view changed openglwidget to bar graph

--- MainUi.py
+++ MainUi.py
@@ -1,30 +1,60 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtGui
 from scriptRunner import runScripts
+from Endpoints import mockup, getDataForText, getDataForGraph
+import numpy as np
+import sys
+
+from matplotlib.backends.qt_compat import QtCore, QtWidgets
+if QtCore.qVersion() >= "5.":
+    from matplotlib.backends.backend_qt5agg import (
+        FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
+else:
+    from matplotlib.backends.backend_qt4agg import (
+        FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
+from matplotlib.figure import Figure
 
 class Ui_MainWindow(object):
 
     def setupUi(self, MainWindow):
         self.textName = ""
+        self.LabelString = ""
         self.tab = []
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1125, 793)
+        MainWindow.resize(1350, 793)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.openGLWidget = QtWidgets.QOpenGLWidget(self.centralwidget)
-        self.openGLWidget.setGeometry(QtCore.QRect(30, 70, 791, 641))
-        self.openGLWidget.setObjectName("openGLWidget")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(900, 530, 131, 51))
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton.clicked.connect(self.openFile)
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(30, 5, 791, 61))
         self.label.setObjectName("label")
-        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.label.setWordWrap(True)
+
+        static_canvas = FigureCanvas(Figure(figsize=(5, 3)))
+
+        networks = ("InceptionResnetV2", "InceptionV3", "MobileNet", "MobileNetV2", "ResNet50", "VGG16","VGG19")
+        performance = (0,0,0,0,0,0,0)
+
+        self._static_ax = static_canvas.figure.subplots()
+        self._static_ax.bar(networks, performance, align='center')
+
+        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(29, 79, 1300, 641))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.horizontalLayout.addWidget(static_canvas)
+        self.pushButton_2 = QtWidgets.QPushButton(self.horizontalLayoutWidget)
         self.pushButton_2.setEnabled(False)
-        self.pushButton_2.setGeometry(QtCore.QRect(900, 620, 131, 51))
+        self.pushButton_2.setMaximumSize(QtCore.QSize(80, 50))
         self.pushButton_2.setObjectName("pushButton_2")
         self.pushButton_2.clicked.connect(self.runApp)
+        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.pushButton = QtWidgets.QPushButton(self.horizontalLayoutWidget)
+        self.pushButton.setEnabled(True)
+        self.pushButton.setMaximumSize(QtCore.QSize(80, 50))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.openFile)
+        self.horizontalLayout.addWidget(self.pushButton)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1125, 26))
@@ -45,7 +75,10 @@ class Ui_MainWindow(object):
 
     def runApp(self):
         self.tab = runScripts(self.textName)
-        self.label.setText("bsfoinvsdkg")
+        tabtemp = getDataForGraph(self.tab)
+        self._static_ax.bar(tabtemp[0], tabtemp[1], align='center')
+        self._static_ax.figure.canvas.draw()
+        self.label.setText(getDataForText(self.tab))
 
     def openFile(self):
         self.textName, selectedFilter = QtWidgets.QFileDialog.getOpenFileName(None, "Wybierz obrazek",  "Początkowa nazwa pliku", "Image files (*.jpg *.png)")
@@ -54,10 +87,9 @@ class Ui_MainWindow(object):
     
 
 if __name__ == "__main__":
-    import sys
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    ex = Ui_MainWindow()
+    w = QtWidgets.QMainWindow()
+    ex.setupUi(w)
+    w.show()
     sys.exit(app.exec_())
