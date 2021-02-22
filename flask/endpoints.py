e5f46f0ced450816ac3f40e9724dca33d9e09a70
Bartosz Skiba <bartski816@student.polsl.pl>
2021-02-22 13:20:38 +0100

    +Login endpoint

--- flask/endpoints.py
+++ flask/endpoints.py
@@ -1,45 +1,28 @@
+import os, sys
+#Użycie parentdir
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
 # Dołączanie modułu flask 
 from flask import Flask
 from flask import render_template, request, redirect, url_for, flash, session
-import sqlite3
 from flask_session import Session
-# Tworzenie aplikacji
-app = Flask("Dogopedia")
-# Ścieżka do pliku bazy danych w sqlite
-DATABASE = 'database.db'
+import dbConnection
+# Tworzenie aplikacji - w tym momencie rootem jest ścieżka Dogopedia więc trzeba zrobić flask/templates
+app = Flask("Dogopedia", template_folder='flask/templates')
 sess = Session()
 
-#Tworzenie baazy danych tylko jesli tabela nie istnieje
-@app.route('/create_db', methods=['GET', 'POST'])
-def create_db():
-    # Połączenie sie z bazą danych
-    conn = sqlite3.connect(DATABASE)
-    # Stworzenie tabeli w bazie danych za pomocą sqlite3 jesli tabela nie istnieje
-    conn.execute('CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, isAdmin INTEGER,ID INTEGER PRIMARY KEY AUTOINCREMENT)')
-    conn.execute('CREATE TABLE IF NOT EXISTS books (author TEXT, title TEXT, ID INTEGER PRIMARY KEY AUTOINCREMENT)')
-    # Zakończenie połączenia z bazą danych
-    conn.close()
 
 #Widok główny
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # if 'user' in session:
-    #     con = sqlite3.connect(DATABASE)
-        
-    #     # Pobranie danych z tabeli
-    #     cur = con.cursor()
-    #     cur.execute("select * from books ORDER BY author ASC")
-    #     books = cur.fetchall()
-    #     con.close()
-    #     adminView = ""
-    #     if 'isAdmin' in session:
-    #         if session['isAdmin'] == 1:
-    #             adminView = "<a href=/users>Użytkownicy</a>"
-    #     return render_template('index.html', books = books, user=session['user']) + adminView
-    # else:
-    #     print("nie ma user w sesji")
-    #     return redirect(url_for('login'))
-    return "Welcome to dogopedia!"
+    if 'user' in session:        
+        # Pobranie danych z tabeli
+        posts = dbConnection.getUserPosts(session['user'])
+        return render_template('index.html', posts = posts, user=session['user'])
+    else:
+        print("nie ma user'a w sesji")
+        return redirect(url_for('login'))
 
 #widok logowanie
 @app.route('/login', methods=['GET', 'POST'])
@@ -52,16 +35,10 @@ def login():
     else:
         login = request.form['login']
         password = request.form['password']
-        con = sqlite3.connect(DATABASE)
-        cur = con.cursor()
-        cur.execute("SELECT * FROM users WHERE username == ? AND password == ?", [login,password])
-        users = cur.fetchall()
-        print(users)
-        if len(users) == 1:
+        id = login(login, password)
+        if id != -1:
             print("jest uzytkownik")
-            session['user'] = users[0][0]
-            session['isAdmin'] = users[0][2]
-        con.close()
+            session['user'] = id
         return redirect(url_for('index'))
 
 #dodanie użytkownika
@@ -72,21 +49,9 @@ def userAdd():
     else:
         userLogin = request.form['login']
         password = request.form['password']
-        administrator = 0
-        if request.form.get('administrator'):
-            administrator = 1
         # Dodanie użytkownika do bazy danych
-        con = sqlite3.connect(DATABASE)
-        cur = con.cursor()
-        cur.execute("SELECT * FROM users WHERE username ==?",[userLogin])
-        users = cur.fetchall()
-        print(administrator)
-        if len(users) > 0:
-            con.close()
-            return "Użytkonik istnieje" + render_template('add.html')
-        cur.execute("INSERT INTO users (username,password, isAdmin) VALUES (?,?,?)",(userLogin,password,administrator) )
-        con.commit()
-        con.close()
+        
+        
         return "Dodano użytkownika do bazy danych <br>" + render_template('login.html')
 
 #dodanie ksiazki
@@ -95,13 +60,7 @@ def bookAdd():
     if 'user' in session:
         title = request.form['title']
         author = request.form['author']
-        # Dodanie użytkownika do bazy danych
-        con = sqlite3.connect(DATABASE)
-        cur = con.cursor()
-        cur.execute("INSERT INTO books (title,author) VALUES (?,?)",(title,author) )
-        con.commit()
-        con.close()
-
+        
         return "Dodano ksiązkę do bazy danych <br>" + index()
     else:
         return "Co ty tutaj robisz?"
@@ -111,42 +70,18 @@ def bookAdd():
 def users():
     if 'isAdmin' in session:
         if session['isAdmin'] == 1:
-            con = sqlite3.connect(DATABASE)
-            cur = con.cursor()
-            cur.execute("SELECT * FROM users")
-            users = cur.fetchall()
-            con.close()
+            
             return render_template('users.html', users=users)
     return "Co ty tutaj robisz? <br> <a href=/>Powrót</a>"
 
 #jeden użytkownik
 @app.route('/user/<int:get_id>',methods=['GET'])
 def get_user_by_id(get_id):
-    if 'isAdmin' in session:
-        if session['isAdmin'] == 1:
-            con = sqlite3.connect(DATABASE)
-            cur = con.cursor()
-            cur.execute("SELECT * FROM users WHERE id == ?",[get_id])
-            user = cur.fetchone()
-            con.close()
-            if user is None:
-                return "Użytkownik nie został znaleziony. <br><a href='/users'>Powrót</a>"
-            else:
-                return render_template('user.html',user=user) 
+        
     return "Co ty tutaj robisz? <br> <a href=/>Powrót</a>"
 @app.route('/user/<get_name>',methods=['GET'])
 def get_user_by_name(get_name):
-    if 'isAdmin' in session:
-        if session['isAdmin'] == 1:
-            con = sqlite3.connect(DATABASE)
-            cur = con.cursor()
-            cur.execute("SELECT * FROM users WHERE username == ?",[get_name])
-            user = cur.fetchone()
-            con.close()
-            if user is None:
-                return "Użytkownik nie został znaleziony. <br><a href='/users'>Powrót</a>"
-            else:
-                return render_template('user.html',user=user)   
+        
     return "Co ty tutaj robisz? <br> <a href=/>Powrót</a>"
 
 #wylogowanie
@@ -157,8 +92,6 @@ def logout():
         session.pop('isAdmin')
     return redirect(url_for('login'))
 
-#Przygotowanie bazy danych jeśli ona nie istnieje (włącznie ze sprawdzeniem tabel)
-create_db()
 # Uruchomienie applikacji w trybie debug
 app.secret_key = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
